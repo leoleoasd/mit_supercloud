ab58aede3ddd539479ed9e8d37dc225cdba610f4
Leo Lu <luyuxuanleo@gmail.com>
2023-11-18 18:21:46 -0500

    update script

--- launch_jupyter.sh
+++ launch_jupyter.sh
@@ -1,6 +1,41 @@
 #!/usr/bin/env bash
-SCRIPT_HOME=/home/gridsan/ylu/scripts
-sbatch_output=$(sbatch $SCRIPT_HOME/jupyter_job.sh)
+# some environment variables for offline use
+export HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 WANDB_MODE=offline
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_HOME=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+sbatch_output=$(sbatch << EOT
+#!/bin/sh
+#SBATCH --job-name=jupyter                                      # Job name
+#SBATCH -p debug-gpu                                            # Partition (queue) name
+#SBATCH --mail-type=ALL                                         # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=lu.yuxuan@northeastern.edu                  # Where to send mail	
+#SBATCH --nodes=1                                               # Number of nodes
+#SBATCH --gpus=1                                                # Number of GPUs to allocate
+#SBATCH --time=01:00:00                                         # Time limit hrs:min:sec
+#SBATCH --output=$SCRIPT_HOME/logs/jupyter%j.log   # Standard output and error log
+
+srun --resv-ports=1 \
+    bash << EOF
+    RANDOM_ID=\\\$(uuidgen | cut -c1-8)
+    PORTAL_FWNAME="\\\$(id -un | tr '[A-Z]' '[a-z]')-\\\$RANDOM_ID-jupyter"
+    PORTAL_FWFILE="/home/gridsan/portal-url-fw/\\\${PORTAL_FWNAME}"
+    echo "Forward name is: \\\${PORTAL_FWNAME}"
+    echo "Portal URL is: https://\\\${PORTAL_FWNAME}.fn.txe1-portal.mit.edu/"
+    echo "http://\\\$(hostname -s):\\\${SLURM_STEP_RESV_PORTS}/" > \\\$PORTAL_FWFILE
+    # cat \$PORTAL_FWFILE
+    chmod u+x \\\${PORTAL_FWFILE}
+    jupyter lab --port \\\${SLURM_STEP_RESV_PORTS} --ip \\\$(hostname -s) --no-browser
+EOF
+EOT
+)
+
 job_id=$(echo "$sbatch_output" | awk '{print $4}')
 echo "JobID: $job_id submitted; waiting for it to start..."
 
@@ -13,17 +48,32 @@ while true; do
     sleep 1
 done
 echo ""
-echo "Job $job_id started"
+echo "Job $job_id started. Waiting for jupyter to start..."
+echo "This may take a while, since all the dependencies are at a network drive and needed to be downloaded."
+job_hostname=$(squeue -j ${job_id} --json | jq -r .jobs\[0\].job_resources.allocated_nodes\[0\].nodename)
 # wait for file to exist
 while [ ! -f $SCRIPT_HOME/logs/jupyter${job_id}.log ]; do sleep 1; done
-# find for "Portal URL is: "
+# find for "Forward name is: "
 while true; do
-    portal_url=$(grep -oP 'Portal URL is: \K.*' $SCRIPT_HOME/logs/jupyter${job_id}.log)
-    if [ "$portal_url" != "" ]; then
-        echo "Portal URL: $portal_url"
+    forward_name=$(grep -oP 'Forward name is: \K.*' $SCRIPT_HOME/logs/jupyter${job_id}.log)
+    if [ "$forward_name" != "" ]; then
         break
     fi
+    echo -ne "."
+    sleep 1
 done
+target_name=$(cat /home/gridsan/portal-url-fw/${forward_name})
+# Find for something like "http://$job_hostname:.../"
+while true; do
+    jupyter_url=$(grep -m1 -oP ".*\Khttp://$job_hostname.*" $SCRIPT_HOME/logs/jupyter${job_id}.log)
+    if [ "$jupyter_url" != "" ]; then
+        break
+    fi
+    echo -ne "."
+    sleep 1
+done
+echo ""
+# replace target name in jupyter_url to PORTAL_FWNAME
+output_url=$(echo $jupyter_url | sed "s|$target_name|https://$forward_name.fn.txe1-portal.mit.edu/|")
+echo "Portal URL is: $output_url"
 
-
-# tail -f logs/jupyter${job_id}.log
